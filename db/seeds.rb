0ebacbd3817fb25f80adba4138a1ffde42246016
rico0072010 <danildolin96@gmail.com>
2020-09-02 23:06:27 +0500

    Add messages with partial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create a first user
+User.create(username: 'Daniel', password: 'foobar')
+
+# Create some users with messages
+(1..5).each do |n|
+  User.create(username: "ExampleUser#{n}", password: 'foobar')
+end
+
+# Create some messages
+(1..4).each do |n|
+  Message.create(body: 'Hello there', user_id: n)
+end
